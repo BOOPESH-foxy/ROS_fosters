7cd9e82308847be0f2d03cf45b0bd67d3319929a
BOOPESH-foxy <boopesh.mc@gmail.com>
2022-10-08 13:59:11 +0530

    cpp node

---
+++ ros_demo_pkg/src/demo_topic_publisher.cpp
@@ -0,0 +1,20 @@
+#include "ros/ros.h"
+#include "std_msgs/Int32.h"
+#include <iostream>
+int main(int argc, char **argv) {
+ros::init(argc, argv,"demo_topic_publisher");
+ros::NodeHandle node_obj;
+ros::Publisher number_publisher = node_obj.advertise<std_
+msgs::Int32>("/numbers", 10);
+ros::Rate loop_rate(10);
+int number_count = 0;
+while ( ros::ok() ) {
+std_msgs::Int32 msg;
+msg.data = number_count;
+ROS_INFO("%d",msg.data);
+number_publisher.publish(msg);
+loop_rate.sleep();
+++number_count;
+}
+return 0;
+}
